05d5513e2f52995fc9fc42aa0c5391e368ea516b
Ahmet Özçelik <109515190+cylinderheadgasket@users.noreply.github.com>
2022-10-11 09:54:48 +0300

    Add files via upload

---
+++ DetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.swift
+//  ciftetelliApp2
+//
+//  Created by Ahmet Özçelik on 6.10.2022.
+//
+
+import UIKit
+
+class DetailViewController: NSObject {
+
+}
+// geliştireceğim hocam
---
+++ ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ciftetelliApp2
+//
+//  Created by Ahmet Özçelik on 6.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var giris: UIButton!
+    @IBOutlet weak var login: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        giris.layer.cornerRadius = 6
+    }
+
+    @IBAction func giris2(_ sender: UIButton) {
+    }
+    
+}
+
